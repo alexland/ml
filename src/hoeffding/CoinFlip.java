aa8ca064071b4e1c6389244bd232557bcdfde51e
Vinay Penmatsa <vinay@penmatsa.org>
2014-01-20 22:27:54 -0500

    Formatting

--- src/hoeffding/CoinFlip.java
+++ src/hoeffding/CoinFlip.java
@@ -28,90 +28,80 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class CoinFlip {
-	
+
 	private double v_1 = 0.0;
 	private double v_rand = 0.0;
 	private double v_min = 0.0;
-	
+
 	// heads - true, tails - false
 	boolean[][] flips = new boolean[1000][10];
 	int[] headCount = new int[1000];
-	
+
 	public CoinFlip(Random rand) {
 		Arrays.fill(headCount, 0);
-				
+
 		int minCount = Integer.MAX_VALUE;
-		for(int i=0; i<1000; i++) {
-			for(int j=0; j<10; j++) {
+		for (int i = 0; i < 1000; i++) {
+			for (int j = 0; j < 10; j++) {
 				boolean nb = rand.nextBoolean();
 				flips[i][j] = nb;
-				if(nb)
-					headCount[i] ++;
+				if (nb)
+					headCount[i]++;
 			}
-			if(headCount[i] < minCount) {
+			if (headCount[i] < minCount) {
 				minCount = headCount[i];
 			}
 		}
-		
+
 		int c_rand = rand.nextInt(1000);
 		boolean[] c_rand_tosses = flips[c_rand];
-		for(int i=0; i<10; i++) {
-			if(c_rand_tosses[i]) v_rand += 1.0; 
+		for (int i = 0; i < 10; i++) {
+			if (c_rand_tosses[i])
+				v_rand += 1.0;
 		}
 		v_rand /= 10.0;
-		
-		for(int i=0; i<10; i++) {
-			if(flips[0][i]) v_1 += 1.0; 
+
+		for (int i = 0; i < 10; i++) {
+			if (flips[0][i])
+				v_1 += 1.0;
 		}
 		v_1 /= 10.0;
-		
-		v_min = minCount/10.0;
+
+		v_min = minCount / 10.0;
 	}
-	
-	
-	
-	
+
 	public double getV_1() {
 		return v_1;
 	}
-
-
-
 
 	public double getV_rand() {
 		return v_rand;
 	}
 
-
-
-
 	public double getV_min() {
 		return v_min;
 	}
-
-
-
 
 	public static void main(String[] args) {
 		double avg_v_1 = 0.0;
 		double avg_v_rand = 0.0;
 		double avg_v_min = 0.0;
-		
+
 		Random r = new Random();
 		// 100000 experiments
-		for(int i=0;i<100000; i++) {
+		for (int i = 0; i < 100000; i++) {
 			CoinFlip cf = new CoinFlip(r);
 			avg_v_1 += cf.getV_1();
 			avg_v_rand += cf.getV_rand();
 			avg_v_min += cf.getV_min();
 		}
-		
+
 		avg_v_1 /= 100000.0;
 		avg_v_rand /= 100000.0;
 		avg_v_min /= 100000.0;
-		
+
 		System.out.println(avg_v_1 + ", " + avg_v_rand + ", " + avg_v_min);
-		
+
 		// v_1 and v_rand should satisfy hoefding's inequality: epsilon ~ .001
 	}
 }
--- src/perceptron/Perceptron.java
+++ src/perceptron/Perceptron.java
@@ -29,45 +29,46 @@ import java.util.Random;
 public class Perceptron {
 
 	public static final int ATTR_COUNT = 2;
-	
+
 	private GUI gui;
 	static Point[] inputs;
 	static int[] outputs;
 	static Line targetLine;
 	private boolean noPaint;
-	
+
 	Random rand = new Random();
-	
+
 	public Perceptron(GUI gui, boolean showGui) {
 		this.gui = gui;
 		this.noPaint = !showGui;
 	}
-	
+
 	public void generateInputs(int size) {
 		inputs = new Point[size];
-		
-		for(int i=0; i<size; i++) {
-			inputs[i] = new Point(getSign() * rand.nextDouble(), getSign() * rand.nextDouble());
+
+		for (int i = 0; i < size; i++) {
+			inputs[i] = new Point(getSign() * rand.nextDouble(), getSign()
+					* rand.nextDouble());
 		}
 	}
-	
+
 	public void generateTargetLine() {
 		double d1 = getSign() * rand.nextDouble();
 		double d2 = getSign() * rand.nextDouble();
-		
-		while(d1 > d2) {
+
+		while (d1 > d2) {
 			d1 = getSign() * rand.nextDouble();
 			d2 = getSign() * rand.nextDouble();
 		}
 		targetLine = new Line(new Point(getSign() * rand.nextDouble(), d1),
 				new Point(getSign() * rand.nextDouble(), d2));
 	}
-	
+
 	private void determineOutput() {
 		outputs = new int[inputs.length];
 		int i = 0;
-		for(Point p : inputs) {
-			if(isLeft(targetLine.getP1(), targetLine.getP2(), p)) {
+		for (Point p : inputs) {
+			if (isLeft(targetLine.getP1(), targetLine.getP2(), p)) {
 				outputs[i] = -1;
 			} else
 				outputs[i] = 1;
@@ -78,62 +79,64 @@ public class Perceptron {
 	private int getSign() {
 		return rand.nextBoolean() ? 1 : -1;
 	}
-	
-	public boolean isLeft(Point a, Point b, Point c){
-	     return ((b.getX() - a.getX())*(c.getY() - a.getY()) - (b.getY() - a.getY())*(c.getX() - a.getX())) > 0;
+
+	public boolean isLeft(Point a, Point b, Point c) {
+		return ((b.getX() - a.getX()) * (c.getY() - a.getY()) - (b.getY() - a
+				.getY()) * (c.getX() - a.getX())) > 0;
 	}
 
 	private int solve() {
 		double[] weights = new double[ATTR_COUNT + 1];
 		Arrays.fill(weights, 0.1);
-		
+
 		Random r = new Random();
 		int count = 0;
-		for(int i=0; i<10000; i++) {
+		for (int i = 0; i < 10000; i++) {
 			int next = r.nextInt(inputs.length);
 			Point xn = inputs[next];
-			
-			double sign = weights[0] + weights[1] * xn.getX() + weights[2] * xn.getY();
-			
-			if((sign < 0 && outputs[next] > 0) || (sign > 0 && outputs[next] < 0)) {
+
+			double sign = weights[0] + weights[1] * xn.getX() + weights[2]
+					* xn.getY();
+
+			if ((sign < 0 && outputs[next] > 0)
+					|| (sign > 0 && outputs[next] < 0)) {
 				// misclassified
 				weights[0] += outputs[next];
 				weights[1] += outputs[next] * xn.getX();
 				weights[2] += outputs[next] * xn.getY();
 				count++;
-				
-				double xx = -(weights[0]/weights[1]);
-				double yy = -(weights[0]/weights[2]);
-				if(!noPaint) {
-					gui.drawHypothesisLine(new Line(new Point(xx, 0), new Point(0, yy)));
+
+				double xx = -(weights[0] / weights[1]);
+				double yy = -(weights[0] / weights[2]);
+				if (!noPaint) {
+					gui.drawHypothesisLine(new Line(new Point(xx, 0),
+							new Point(0, yy)));
 					try {
 						Thread.sleep(200);
 					} catch (InterruptedException e) {
 					}
 				}
 			}
-			
+
 		}
-		
+
 		return count;
 	}
 
-	
 	public static void main(String[] args) {
 		GUI g = new GUI("Perceptron Learning");
 
 		boolean showGui = true;
-		if(args.length == 1) {
-			if("nogui".equals(args[0]))
+		if (args.length == 1) {
+			if ("nogui".equals(args[0]))
 				showGui = false;
 		}
 		Perceptron p = new Perceptron(g, showGui);
-		
+
 		p.generateInputs(100);
 		p.generateTargetLine();
 		p.determineOutput();
 
-		
 		g.show();
 		g.drawInputs(inputs);
 		g.drawTargetLine(targetLine);
@@ -147,20 +150,20 @@ public class Perceptron {
 class Point {
 	private double x;
 	private double y;
-	
+
 	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "(" + x + "," + y + ")";
@@ -170,20 +173,20 @@ class Point {
 class Line {
 	private Point p1;
 	private Point p2;
-	
+
 	public Line(Point p1, Point p2) {
 		this.p1 = p1;
 		this.p2 = p2;
 	}
-	
+
 	public Point getP1() {
 		return p1;
 	}
-	
+
 	public Point getP2() {
 		return p2;
 	}
-	
+
 	@Override
 	public String toString() {
 		return p1 + " - " + p2;
